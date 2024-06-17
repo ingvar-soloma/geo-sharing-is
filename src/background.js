b66f9136a89ab33a4c8b692b8cbbc96cd53b7447
ingvar-soloma <ingvar.soloma@gmail.com>
2024-06-17 02:30:15 +0000

    background sending geolocation

--- src/background.js
+++ src/background.js
@@ -1,41 +1,27 @@
-// const TelegramBotService = require("@/services/TelegramBotServiceB");
-
-
-addEventListener("syncTest", (resolve, reject, args) => {
-  console.log("calling sending");
+addEventListener("syncTest", async (resolve, reject, args) => {
+  const location = await CapacitorGeolocation.getCurrentPosition();
 
   const botToken = '7192431573:AAGtLVuTNAscrKPJlTI_n6ANmYmlWCrM0Fc';
   const chatId = '518383100';
-  const message = 'Hello from your bot! B';
 
-  // const telegramBot = new TelegramBotService(botToken, chatId.value);
-  // telegramBot.sendMessage(message).then(r =>  {})
+  await sendLocation(botToken, chatId, location.latitude, location.longitude)
 
-  setTimeout(() => {
-    console.log("tick B");
-
-    resolve();
-  }, 3000);
+  resolve();
 });
 
-addEventListener("syncTestF", (resolve, reject, args) => {
-  console.log("calling sync syncTestF");
+addEventListener("syncTestF", async (resolve, reject, args) => {
+
+  const location = await CapacitorGeolocation.getCurrentPosition();
 
   const botToken = '7192431573:AAGtLVuTNAscrKPJlTI_n6ANmYmlWCrM0Fc';
   const chatId = '518383100';
-  const message = 'Hello from your bot! F';
 
-  // const telegramBot = new TelegramBotService(botToken, chatId.value);
-  // telegramBot.sendMessage(message).then(r =>  {})
+  sendLocation(botToken, chatId, location.latitude, location.longitude)
 
-  setTimeout(() => {
-    console.log("tick syncTestF");
-    resolve();
-  }, 3000);
+  resolve();
 });
 
 addEventListener("asyncTest", async (resolve, reject, args) => {
-  console.log("calling async test");
   await new Promise((res) => {
     setTimeout(() => {
       console.log("tick");
@@ -45,3 +31,102 @@ addEventListener("asyncTest", async (resolve, reject, args) => {
   console.log("returning async result");
   resolve();
 });
+
+// addEventListener("testCapNotification", async (resolve, reject, args) => {
+//   try {
+//     let scheduleDate = new Date();
+//     scheduleDate.setSeconds(scheduleDate.getSeconds() + 60);
+//
+//     CapacitorNotifications.schedule([
+//       {
+//         id: 100,
+//         title: "Enterprise Background Runner",
+//         body: "A test message from the Enterprise Background Runner",
+//         scheduleAt: scheduleDate,
+//       },
+//     ]);
+//
+//     resolve();
+//   } catch (err) {
+//     console.error(err);
+//     reject(err);
+//   }
+// });
+
+addEventListener("testCapacitorGeolocation", async (resolve, reject, args) => {
+  try {
+    const location = await CapacitorGeolocation.getCurrentPosition();
+    console.log("current location: " + JSON.stringify(location));
+    resolve(location);
+  } catch (err) {
+    console.error(err);
+    reject(err);
+  }
+});
+
+async function sendMessage(botToken, chatId, message) {
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message,
+      parse_mode: 'html'
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+async function sendLocation(botToken, chatId, latitude, longitude) {
+  try {
+    const baseUrl = `https://api.telegram.org/bot${botToken}/sendLocation`;
+
+    const params = {
+      chat_id: chatId.toString(),
+      latitude: latitude.toString(),
+      longitude: longitude.toString(),
+      live_period: '3600',
+      horizontal_accuracy: '100',
+      protect_content: 'true',
+      disable_notification: 'true',
+      // heading: '90',
+      // title: 'test',
+      // address: 'testadr'
+    };
+
+    const queryString = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+      .join('&');
+
+    console.log('teststr');
+    console.log(`${baseUrl}?${queryString}`);
+
+    const response = await fetch(`${baseUrl}?${queryString}`, {
+      method: 'GET',
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log('Location sent successfully:', data);
+  } catch (error) {
+    console.error('Error sending location:', error.message);
+    throw new Error('Error sending location');
+  }
+}
+
+// export async function getCurrentPosition() {
+//   const coordinates = await Geolocation.getCurrentPosition();
+//   return coordinates.coords;
+// }
