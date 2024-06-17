6e9dd62bcb7c2cd41234d31f71671e0d85a000bb
ingvar-soloma <ingvar.soloma@gmail.com>
2024-06-17 00:57:02 +0000

    background vite build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,19 @@ export default defineConfig({
     vue(),
     legacy()
   ],
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       background: path.resolve(__dirname, 'src/background.js')
+  //     },
+  //     output: {
+  //       format: 'module',
+  //       dir: 'android/app/src/main/assets/public/b', // Specify the output directory
+  //       entryFileNames: '[name].js' // Specify the entry file names
+  //     }
+  //   },
+  //   modulePreload: false
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
